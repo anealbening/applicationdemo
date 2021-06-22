84aafa5739d91094bd78593d2553f5f218a4b12c
Aneal Bening on Mac <anealbening@hotmail.co.uk>
2021-06-22 15:35:25 +0100

    Added a console log message to test console was working. Also changed paragraph.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 const App = () => {
+  console.log('Hello from component. This is Aneal here.')
   return (
     <div>
-      <p>Hello world this is Aneal's app</p>
+      <p>Hello world this is Aneal here.</p>
     </div>
   )
 }
