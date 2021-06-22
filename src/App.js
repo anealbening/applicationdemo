921625e9136405d1259cd6fff0b7df703d4c7c2e
Aneal Bening on Mac <anealbening@hotmail.co.uk>
2021-06-22 22:01:40 +0100

    Added in props to the code. Passed in 'Aneal' and 'Bening' as name props.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react'
-
-const Hello = () => {
+/* Code written by Aneal Bening. June 2021. Followed tutorial on fullstackopen.com (2021 tutorial).*/
+const Hello = (props) => {
   return (
     <div>
-      <h1>Hello world this is shown from 'Hello' constant</h1>
-    </div>
+      <h1>Hello world {props.name} this is shown from 'Hello' constant</h1> 
+    </div> /* Implemented props into code. */
   )
 }
 
 
-
-
-
-const App = () => {
+const App = () => { /* This performs addition dynamically within the app and also passes in name through props in Hello constant.*/
   const now = new Date()
-  const a = 10
+  const a = 10 
   const b = 20
   return (
     <div>
@@ -23,8 +20,9 @@ const App = () => {
         {a} + {b} is {a + b}
       </p>
       <p>This is an example of the ability to render dynamic content inside of a component within React.</p>
-      <p>Created by Aneal Bening.</p>
-      <Hello />
+      <p>Created by Aneal Bening.</p> 
+      <Hello name="Aneal" /> 
+      <Hello name="Bening" />
     </div>
   )
 }
