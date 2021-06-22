115277e7e91379086b29eabc3f6824ea3caede3f
Aneal Bening on Mac <anealbening@hotmail.co.uk>
2021-06-22 15:43:11 +0100

    Removed the console log message and implemented dynamic addition to display to the user.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 const App = () => {
-  console.log('Hello from component. This is Aneal here.')
+  const now = new Date()
+  const a = 10
+  const b = 20
   return (
     <div>
       <p>Hello world this is Aneal here.</p>
+      <p>
+        {a} + {b} is {a + b}
+      </p>
+      <p>This is an example of the ability to render dynamic content inside of a component within React.</p>
     </div>
   )
 }
