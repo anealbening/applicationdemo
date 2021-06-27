2e98e844a2b570193953c4a4cb868fda40a65337
Aneal Bening on Mac <anealbening@hotmail.co.uk>
2021-06-27 20:41:30 +0100

    Added constant to test my knowledge on implementing a constant variable which can be retrieved.

--- src/App.js
+++ src/App.js
@@ -12,12 +12,14 @@ const Hello = (props) => {
 const App = () => { /* Demonstrates how values (e.g 'Name' and 'age' values) can be passed through so they can be used for props.*/
   const name = 'Peter'
   const age = 10
+  const test = 'Name'
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name="Aneal Bening" age={20} /> 
       <Hello name={name} age={age} />
       <Footer /> 
+      <p>Retrieving test constant variable {test}</p> 
     </div>
   )
 }
