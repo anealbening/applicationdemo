e90f1d93a21d7a646fc23ec0d4e3052eccd8e34c
Aneal Bening on Mac <anealbening@hotmail.co.uk>
2021-06-28 15:52:22 +0100

    Implemented guessing year of birth into function - will look at age value.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 /* Code written by Aneal Bening. June 2021. Followed tutorial on fullstackopen.com (2021 tutorial).*/
-const Hello = (props) => {
+const Hello = (props) => { /*Implemented guessing year of birth into code */
   const bornYear =() => {
     const yearNow = new Date().getFullYear()
     return yearNow - props.age
@@ -8,7 +8,7 @@ const Hello = (props) => {
   return (
     <div>
       <h1>Hello {props.name}, you are {props.age} years old.</h1> 
-      <p>So you were probably born in {bornYear()}</p>
+      <p>So you were probably born in {bornYear()}</p> 
     </div> /* Implemented props into code. */
   )
 }
@@ -17,12 +17,10 @@ const Hello = (props) => {
 const App = () => { /* Demonstrates how values (e.g 'Name' and 'age' values) can be passed through so they can be used for props.*/
   const name = 'Peter'
   const age = 10
-  const test = 'Name'
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name="Aneal Bening" age={20} /> 
-      <Hello name={name} age={age} />
       <Footer /> 
       <p>Retrieving test constant variable: {test}</p> 
     </div>
