65f956b336bfbd9771a3ae5baccc43ccd8d3e3a6
Aneal Bening on Mac <anealbening@hotmail.co.uk>
2021-06-30 17:58:17 +0100

    Introduced destructuring to improve code.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 /* Code written by Aneal Bening. June 2021. Followed ReactJS tutorial on fullstackopen.com (2021 tutorial).*/
 const Hello = (props) => { /*Implemented guessing year of birth into code */
-  const bornYear =() => {
+  const name = props.name /* created variable which assigns constant 'name' to props.name (destructuting) */
+  const age = props.age /* created variable which assigns constant 'age' to props.name (destructuring) */
+  const bornYear =() => { 
     const yearNow = new Date().getFullYear()
-    return yearNow - props.age
+    return yearNow - age
   }
   return (
     <div>
-      <h1>Hello {props.name}, you are {props.age} years old.</h1> 
+      <h1>Hello {name}, you are {age} years old.</h1> 
       <p>So you were probably born in {bornYear()}</p> 
     </div> /* Implemented props into code. */
   )
