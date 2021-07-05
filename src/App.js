19f6a8263766c3dfe52ab7be667bf8a13824e902
Aneal Bening on Mac <anealbening@hotmail.co.uk>
2021-07-05 21:44:36 +0100

    Implemented page re-rendering using a counter.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import React from 'react'
-/* Code written by Aneal Bening. June 2021. Followed ReactJS tutorial on fullstackopen.com (2021 tutorial).*/
-const Hello = (props) => { /*Implemented guessing year of birth into code */
-  const name = props.name /* created variable which assigns constant 'name' to props.name (destructuting) */
-  const age = props.age /* created variable which assigns constant 'age' to props.name (destructuring) */
-  const bornYear =() => { 
-    const yearNow = new Date().getFullYear()
-    return yearNow - age
-  }
+
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <h1>Hello {name}, you are {age} years old.</h1> 
-      <p>So you were probably born in {bornYear()}</p> 
-    </div> /* Implemented props into code. */
+    <div>{counter}</div>
   )
 }
-
-
-const App = () => { /* Demonstrates how values (e.g 'Name' and 'age' values) can be passed through so they can be used for props.*/
-  /*const name = 'Peter' */
-  /*const age = 50*/ /* Commented out these two unused variables - for display purposes only. */
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Aneal Bening" age={20} /> 
-      <Footer /> 
-    </div>
-  )
-}
-
-const Footer = () => { /*Footer area which will be at the bottom of the React web page */
-  return (
-    <div>
-      app created by <a href="https://github.com/anealbening">Aneal Bening</a> 
-    </div>
-  )
-}
-
 
 export default App
